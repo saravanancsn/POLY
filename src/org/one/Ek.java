35f897cfc08e8bf69580150f03dc498ce3a9fd1d
saravanancsn <saravanan4896@gmail.com>
2019-11-15 16:18:26 +0530

    POLYMORPHISM FOR CONFLICTS

---
+++ src/org/one/Ek.java
@@ -0,0 +1,10 @@
+package org.one;
+
+public class Ek {
+	public void empId(String a,int b,float c) {
+	System.out.println(a+b+c);
+}public static void main(String[] args) {
+	Ek e=new Ek();
+	e.empId("Abb@2#$543", 1111111, 2222.888f);
+}
+}
---
+++ src/org/one/Two.java
@@ -0,0 +1,17 @@
+package org.one;
+
+public class Two {
+	public void allF(int a) {
+	System.out.println(a);
+}public void allF(String b) {
+	System.out.println("aaa@dr#");
+}public void allF(String c,float d) {
+System.out.println(c+d);
+}public static void main(String[] args) {
+	Two g=new Two();
+	g.allF("abA@jjj");
+	g.allF("as12",111.34f);
+	
+	
+}
+}
